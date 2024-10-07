61b4d9a730451568b1b12b98d7ae577a79f16ddd
Anshika <mishar08anshika@gmail.com>
2024-10-07 12:10:35 +0530

    test submit anshika mishra

--- program1.js
+++ program1.js
@@ -1,8 +1,40 @@
 const getTotalIsles = function (grid) {
+  if (!grid || grid.length === 0) return 0;
+  
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let islandCount = 0;
 
+  
+  function dfs(row, col) {
+    
+    if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] === 'W') {
+      return;
+    }
 
-  // write your code here
+  
+    grid[row][col] = 'W';
+    
+   
+    dfs(row - 1, col);  
+    dfs(row + 1, col);  
+    dfs(row, col - 1);  
+    dfs(row, col + 1);  
+  }
 
+  
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 'L') {
+        
+        islandCount++;
+
+        dfs(i, j);
+      }
+    }
+  }
+
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,40 @@
 const decodeTheRing = function (s, p) {
+  const m = s.length;
+  const n = p.length;
 
-    // write your code here
-
-  };
   
-  module.exports = decodeTheRing;
+  const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(false));
+
+  
+  dp[0][0] = true;
+
+  
+  for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === '*') {
+          dp[0][j] = dp[0][j - 1];
+      } else {
+          break; 
+      }
+  }
+
+ 
+  for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+          if (p[j - 1] === '*') {
+            
+              dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+          } else if (p[j - 1] === '?') {
+              
+              dp[i][j] = dp[i - 1][j - 1];
+          } else {
+              
+              dp[i][j] = s[i - 1] === p[j - 1] && dp[i - 1][j - 1];
+          }
+      }
+  }
+
+
+  return dp[m][n];
+};
+
+module.exports = decodeTheRing;
